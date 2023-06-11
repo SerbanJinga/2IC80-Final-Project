bac9d88e12a34672c434000c5b12cedec32016de
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2023-06-11 16:35:34 +0200

    Renaming files

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import threading
-import dns_test
-import arp_test
+import dns_spoofing
+import arp_spoofing
 import time
 
 def run_arp():
-    arp_test.main()
+    arp_spoofing.main()
 
 def run_dns():
-    dns_test.main()
+    dns_spoofing.main()
 
 if __name__ == "__main__":
     arpThread = threading.Thread(target=run_arp)
